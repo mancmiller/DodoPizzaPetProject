d46cba61a82e4af6d21fa15f284bf64b94349131
mancmiller <manc.miller@gmail.com>
2023-06-26 15:29:50 +0600

    Initial Commit

---
+++ DodoPizzaPetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DodoPizzaPetProject
+//
+//  Created by Muslim on 26.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
