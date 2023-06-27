ba54ed294e2fcefc66fd50418427e03ca5a7e6f8
mancmiller <manc.miller@gmail.com>
2023-06-27 20:45:49 +0600

    Created Model

---
+++ DodoPizzaPetProject/Models/ProductEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ProductEntity+CoreDataClass.swift
+//  DodoPizzaPetProject
+//
+//  Created by Muslim on 27.06.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ProductEntity)
+public class ProductEntity: NSManagedObject {
+
+}
